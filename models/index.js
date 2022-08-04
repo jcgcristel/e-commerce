90cbc4cf6c1ee31403eead504faadc1abb0836d5
jcg <jcg.cristel@gmail.com>
2022-08-04 02:36:58 -0400

    added references to product and tag for product tag

--- models/index.js
+++ models/index.js
@@ -12,11 +12,21 @@ Product.belongsTo(Category, {
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id'
-})
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'product_tag',
+  foreignKey: 'product_id'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'product_tag',
+  foreignKey: 'tag_id'
+});
 
 module.exports = {
   Product,
